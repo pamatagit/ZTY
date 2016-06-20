7e4ca74cf81d639d5a8d9d6c18091f910fb088da
Tony Cheng <tony.pig@gmail.com>
2016-06-20 11:34:52 +0800

    Update nova_qemu_upgrade.pl

--- nova_qemu_upgrade.pl
+++ nova_qemu_upgrade.pl
@@ -5,7 +5,7 @@
 #
 # Edit by Tony Cheng
 # Update : 20160615
-# Version : v0.1
+# Version : v0.3
 # Node : compute node
 # Usage : nova_qemu_upgrade.pl
 # ----------------------------
@@ -20,23 +20,25 @@ chomp($datetime = `/bin/date +'%F'`);
 chomp($datetimes = `/bin/date +'%F %H:%M:%S'`);
 
 #$base_dir = '/root/tony/';
-$base_dir = '/root/tony/qemu_nova-patch';
+$base_dir = '/root/qemu_nova-patch';
 $backup_dir = $base_dir;
 $patch_dir = $base_dir.'/nova/';
 $patch_pkg = 'nova-v1.0.3.tar.gz';
 $python_dist_dir = '/usr/local/lib/python2.7/dist-packages';
 $nova_dir = $python_dist_dir.'/nova';
 $nova_egg_dir = $python_dist_dir.'/nova*egg-info';
-$computer_daemon_name = 'chrome';
-$controller_daemon_name = 'chrame';
+$computer_daemon_name = 'nova-compute';
+$controller_daemon_name = 'nova-schedule';
 $log_file = $base_dir.'/'.'nova_qemu_upgrade.log';
 
 @check_dir = ($base_dir, $python_dist_dir);
 
 # Server vars:
 $repo_server = '192.168.202.250';
-$_ = `ps ax|grep chrome|head -n 1`;
-@nova_daemon = split(/\s+/, $_);
+#$_ = `ps ax|egrep "$computer_daemon_name|$controller_daemon_name"|sed -e "s/^.*nova/nova/"|head -n 1|awk '{print $1}'`;
+$_ = `ps ax|egrep "$computer_daemon_name|$controller_daemon_name"|tail -1`;
+print "$_\n";
+$nova_daemon = $_;
 
 # RunCommand vars:
 $nova103_step1_str = "Step 1/5 : Backup nova v1.0.1 packages...";
@@ -61,10 +63,6 @@ $backup_nova_cmd = "cd $nova_dir; tar zcvf $backup_dir/nova-backup-$datetime.tar
 $unpack_patch_cmd = "cd $base_dir && tar zxvf $patch_pkg";
 $remove_egg_cmd = "cd $python_dist_dir; rm -rf $nova_egg_dir";
 $pip_cmd = "cd $patch_dir && pip install -r requirements.txt; python setup.py install";
-($role eq 'computer') ? ($restart_service_cmd = "service nova-compute restart") : 
-                           ($restart_service_cmd = "service nova-api restart; sleep(3); \
-                                                    service nova-scheduler restart; sleep(3) \
-                                                    service nova-conductor restart");
 #@nova103_allsteps_cmd = ($backup_nova_cmd, $unpack_patch_cmd, $remove_egg_cmd, $pip_cmd);
 
 $renew_sourceslist_cmd = "cp $base_dir/sources.list.nova /etc/apt/sources.list; apt-get update";
@@ -81,14 +79,19 @@ foreach $_ (@check_dir) {
     if (!(-e $_)) {
         die "the directory : $_ is not exists!\n";
     }
-    if ($nova_daemon[5] =~ /$computer_daemon_name/) {
+    if ($nova_daemon =~ /$computer_daemon_name/) {
         $role = 'computer';
-    } elsif ($nova_daemon[5] =~ /$controller_daemon_name/) {
+    } elsif ($nova_daemon =~ /$controller_daemon_name/) {
         $role = 'controller';
     } else {
         die("Error: The role is undefined!\n");
     }
 }
+
+($role eq 'computer') ? ($restart_service_cmd = "service nova-compute restart") : 
+                        ($restart_service_cmd = "service nova-api restart; \
+                                                 service nova-scheduler restart; \
+                                                 service nova-conductor restart");
 
 print "Begin upgrade nova v1.0.3......\nRole : $role\n";
 print "Checking ok!! Running nova v1.0.3 upgrade procedures...\n";
@@ -97,32 +100,32 @@ sleep(6);
 # run commands
 print "\n$nova103_step1_str\n";
 print "command: $backup_nova_cmd\n";
-#die ("Error: $?\n") if (!(system($backup_nova_cmd) == 0));
+die ("Error: $?\n") if (!(system($backup_nova_cmd) == 0));
 sleep(4);
 
 print "\n$nova103_step2_str\n";
 print "command: $unpack_patch_cmd\n";
-#die ("Error: $?\n") if (!(system($unpack_patch_cmd) == 0));
+die ("Error: $?\n") if (!(system($unpack_patch_cmd) == 0));
 sleep(4);
 
 print "\n$nova103_step3_str\n";
 print "command: $remove_egg_cmd\n";
-#die ("Error: $?\n") if (!(system($remove_egg_cmd) == 0));
+die ("Error: $?\n") if (!(system($remove_egg_cmd) == 0));
 sleep(4);
 
 print "\n$nova103_step4_str\n";
 print "command: $pip_cmd\n";
-#die ("Error: $?\n") if (!(system($pip_cmd) == 0));
+die ("Error: $?\n") if (!(system($pip_cmd) == 0));
 sleep(4);
 
 print "\n$nova103_step5_str\n";
 print "command: $restart_service_cmd\n";
-#die ("Error: $?\n") if (!(system($restart_service_cmd) == 0));
+die ("Error: $?\n") if (!(system($restart_service_cmd) == 0));
 sleep(4);
 
 $nova_result = `cat /usr/local/lib/python2.7/dist-packages/nova/virt/vmwareapi/vm_util.py|head -n 171|tail -1`;
 print "checking if vm_util.py in line 171 is : $nova_result\n";
-#die ("Error: patch failed!  $?\n") until ($nova_result =~ /config_spec.name = instance.hostname/);
+die ("Error: patch failed!  $?\n") until ($nova_result =~ /config_spec.name = instance.hostname/);
 
 print "\n\n ===========================================\n\n";
 print "The patch works! ^_^  upgrade nova v1.0.3 successful!!\n\n\n\n";
